a87ba85619bbe9e88878fe49597751c9ee6ec851
agus2023 <landeira2017@gmail.com>
2025-04-14 09:54:37 -0300

    agregando signals a mi servicio github.service

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,23 +9,8 @@ export class GithubService {
 
   httpClient = inject(HttpClient)
   nombre : string = ""
-  resultado ?: any 
-  constructor() {
-
-    // //devuelve la peticion en si como observable
-    // //se ejecuta solamente cuando nos suscribamos y ahi nos da la respuesta
-    // const promesa : Observable<any> = this.httpClient.get("https://api.github.com/users/AgustinLandeira")
-
-    // //con esto obtenemos la respuesta ya que nos suscribimos
-    // //cuando termine mande a esta funcion como param la respuesta(definimos la accion al recibir respuesta)
-    // const suscripcion = promesa.subscribe((respuesta:any)=>{
-
-    //   this.resultado = respuesta
-    //   //console.log(this.resultado)
-    //   suscripcion.unsubscribe()
-
-    // })
-  }
+  resultado = signal<any | null>(null) ;// El valor de la signal se setea con el método .set(valor).
+  constructor() {  }
   
   traer(){
     //devuelve la peticion en si como observable
@@ -36,7 +21,7 @@ export class GithubService {
     //cuando termine mande a esta funcion como param la respuesta(definimos la accion al recibir respuesta)
     const suscripcion = promesa.subscribe((respuesta:any)=>{
 
-      this.resultado = respuesta
+      this.resultado.set(respuesta)
       console.log(this.resultado)
       suscripcion.unsubscribe()
 
@@ -46,18 +31,3 @@ export class GithubService {
 
 }
 
-/*
-
- traerPorNombre(nombre: string) {
-    // Crear petición
-    const peticion: Observable<any> = this.httpClient.get<any>("https://pokeapi.co/api/v2/pokemon/" + nombre)
-
-    // Suscribirse y definir acción al recibir respuesta
-    const suscripcion: Subscription = peticion.subscribe((respuesta) => {
-      this.pokemonActual = respuesta; 
-
-      // Cerrar la suscripción
-      suscripcion.unsubscribe();
-    });
-
-*/
