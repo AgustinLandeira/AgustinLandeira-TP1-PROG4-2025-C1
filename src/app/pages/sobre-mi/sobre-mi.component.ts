b40868c379ad2426ccf5a6cd91c01e5cafad616b
agus2023 <landeira2017@gmail.com>
2025-05-13 12:11:56 -0300

    corrigiendo errores chicos

--- src/app/pages/sobre-mi/sobre-mi.component.ts
+++ src/app/pages/sobre-mi/sobre-mi.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { GithubService } from '../../services/github.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sobre-mi',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sobre-mi.component.html',
   styleUrl: './sobre-mi.component.css'
 })
